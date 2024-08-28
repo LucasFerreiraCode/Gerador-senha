40037adfab330cd7fe28e7b61b0b12a2a5482244
LucasFerreira92 <lucasferreirasurf13@gmail.com>
2024-08-28 14:23:16 -0300

    slider-ajuste

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // Componentes
 let campoSenha = document.getElementById('campo-senha');
-let botao = document.getElementById('botao-gerar');
+let botaoGerar = document.getElementById('botao-gerar'); // Corrigido para 'botaoGerar'
 let botaoCopiar = document.getElementById('botao-copiar');
 let slider = document.getElementById('slider');
 let sliderValue = document.getElementById('slider-value');
-let regSenha =  '!@#$%&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@#$%&*^'
+let regSenha = '!@#$%&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@#$%&*^'
 
-      // Função para atualizar o valor exibido do slider
-      function updateSliderValue(value) {
-        sliderValue.textContent = value;
-      }
+// Função para atualizar o valor exibido do slider
+function updateSliderValue(value) {
+    sliderValue.textContent = value;
+}
 
-botao.addEventListener('click', ()=>{
-
-  let senhaAtual = ''
-  for (let i = 0; i <10; i++){
-      senhaAtual +=regSenha [Math.floor(Math.random() *regSenha.length)]
-  }
-
-  campoSenha.value = senhaAtual
-
+// Evento de clique para gerar senha
+botaoGerar.addEventListener('click', () => {
+    let senhaAtual = '';
+    let senhaLength = parseInt(slider.value);
+    for (let i = 0; i < senhaLength; i++) {
+        senhaAtual += regSenha[Math.floor(Math.random() * regSenha.length)];
+    }
+    campoSenha.value = senhaAtual;
 });
 
+// Evento de clique para copiar senha
 botaoCopiar.addEventListener('click', () => {
     campoSenha.select(); // Seleciona o texto do campo
     navigator.clipboard.writeText(campoSenha.value).then(() => {
@@ -29,4 +29,4 @@ botaoCopiar.addEventListener('click', () => {
     }).catch(err => {
         console.error('Erro ao copiar para a área de transferência: ', err);
     });
-  });
+});
